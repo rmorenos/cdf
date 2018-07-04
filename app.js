2887d319c116a63e56331429c75d074e740805fa
Administrator <1483319587@qq.com>
2018-07-04 23:42:53 +0800

    json入参

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -45,7 +45,7 @@ app.use(function (req, res, next) {
 app.use(cors({
 	origin:['http://localhost:8080'],
 	methods:['GET','POST'],
-	alloweHeaders:['Conten-Type', 'Authorization']
+	alloweHeaders:['Content-Type', 'Authorization', 'Cache-Control', 'no-cache, no-store, must-revalidate']
 }));
 
 //  error handler
