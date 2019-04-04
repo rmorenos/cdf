8b22c62c8d65bdfb117e7ad7a614f4751f3b7003
darks <raul.moreno@solucionesorion.com>
2019-04-03 22:39:39 -0300

    ggggggggg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Expressssss' });
+	res.render('index', { title: 'Soluciones orion' });
 });
 
 module.exports = router;
