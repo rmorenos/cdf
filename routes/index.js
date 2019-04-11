d2818e8943bdfd59dc4a42617f852f8d244de92e
Pofi <raul.moreno@solucionesorion.com>
2019-04-11 12:54:40 -0400

    dddd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Soluciones orion SSGG este si' });
+	res.render('index', { title: 'Soluciones orion SSGG' });
 });
 
 module.exports = router;
